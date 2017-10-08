528e22750683349c28ff5028be0a5ed1eff17f9a
Wei Tung Chen <lleeoochen@gmail.com>
2017-10-08 16:33:44 -0700

    Added movement boundary check

--- RPG-2D-1/main.cpp
+++ RPG-2D-1/main.cpp
@@ -3,14 +3,18 @@
 #include <math.h>
 
 const float SPEED = 0.3;
+const int WIDTH = 700;
+const int HEIGHT = 600;
 void handleMovement(sf::CircleShape& player);
 
+//Game driver
 int main() {
 
 	//Basic setup
-	sf::RenderWindow window(sf::VideoMode(700, 600), "RPG-2D Game!", sf::Style::Titlebar | sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "RPG-2D Game!", sf::Style::Titlebar | sf::Style::Default);
 	sf::CircleShape player(30.f);
-	player.setFillColor(sf::Color::Green);
+	player.setFillColor(sf::Color::Blue);
+	player.setOrigin(player.getRadius(), player.getRadius());
 
 	//Game loop
 	while (window.isOpen()) {
@@ -38,14 +42,14 @@ int main() {
 void handleMovement(sf::CircleShape& player) {
 	sf::Vector2f velocity(0.f, 0.f);
 
-	//Four movement direction
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	//Four movement direction and boundary check
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player.getPosition().x - player.getRadius() >= 0)
 		velocity.x -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player.getPosition().x + player.getRadius() <= WIDTH)
 		velocity.x += SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y - player.getRadius() >= 0)
 		velocity.y -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y + player.getRadius() <= HEIGHT)
 		velocity.y += SPEED;
 
 	//Diagonal movement (/sqrt(2))
