a979baa32ec42617fadf3f670ebb8bb4f7952990
Wei Tung Chen <lleeoochen@gmail.com>
2017-10-08 23:56:53 -0700

    Added bullet!

--- RPG-2D-1/main.cpp
+++ RPG-2D-1/main.cpp
@@ -3,32 +3,43 @@
 #include <math.h>
 #include "Player.h"
 
+const float PI = 3.1415926;
 const float SPEED = 0.5;
+const float BULLET_SPEED = 1;
 const int WIDTH = 960;
 const int HEIGHT = 540;
-void handleMovement(sf::Sprite& player);
+bool AVAILABLE = true;
+std::vector<Player> bullets;
+
+void handleMovement(Player& player);
+void handleRotation(Player& player, sf::RenderWindow& window);
+void handleBullet(Player& player, sf::RenderWindow& window, sf::Texture& texture, int time);
 sf::Texture getTexture(std::string filename);
 
-//Game driver
 int main() {
 
 	//Setup window
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Galaxy Game!", sf::Style::Titlebar | sf::Style::Default);
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Disk Galaxy", sf::Style::Titlebar | sf::Style::Default);
 
 	//Setup textures
 	sf::Texture textureUFO = getTexture("ufo.png");
 	sf::Texture textureGalaxy = getTexture("galaxy.png");
+	sf::Texture textureBullet = getTexture("bullet.png");
 
-	//Setup background
+	//Setup players
 	Player background(textureGalaxy);
-
-	//Setup player
 	Player player(textureUFO);
 	player.setOrigin(player.getWidth() / 2, player.getHeight() / 2);
 	player.setPosition(WIDTH / 2, HEIGHT - player.getHeight() / 2);
 
+	sf::Clock clock;
+
 	//Game loop
 	while (window.isOpen()) {
+
+		//Clear window
+		window.clear();
+		window.draw(background);
 
 		//Event loop
 		sf::Event event;
@@ -37,12 +48,12 @@ int main() {
 				window.close();
 		}
 
-		//Control movement
+		//Control movement and rotation
 		handleMovement(player);
+		handleRotation(player, window);
+		handleBullet(player, window, textureBullet, clock.getElapsedTime().asMilliseconds());
 
 		//Draw objects
-		window.clear();
-		window.draw(background);
 		window.draw(player);
 		window.display();
 	}
@@ -50,18 +61,20 @@ int main() {
 	return 0;
 }
 
+
+
 //Control player movement
-void handleMovement(sf::Sprite& player) {
+void handleMovement(Player& player) {
 	sf::Vector2f velocity(0.f, 0.f);
 
 	//Four movement direction and boundary check
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player.getPosition().x - player.getGlobalBounds().width / 2 >= 0)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player.getPosition().x - player.getWidth() / 2 >= 0)
 		velocity.x -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player.getPosition().x + player.getGlobalBounds().width / 2 <= WIDTH)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player.getPosition().x + player.getWidth() / 2 <= WIDTH)
 		velocity.x += SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y - player.getGlobalBounds().height / 2 >= 0)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y - player.getHeight() / 2 >= 0)
 		velocity.y -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y + player.getGlobalBounds().height / 2 <= HEIGHT)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y + player.getHeight() / 2 <= HEIGHT)
 		velocity.y += SPEED;
 
 	//Diagonal movement (/sqrt(2))
@@ -70,6 +83,69 @@ void handleMovement(sf::Sprite& player) {
 
 	//Move players
 	player.move(velocity);
+}
+
+//Control player rotation
+void handleRotation(Player& player, sf::RenderWindow& window) {
+	sf::Vector2f playerPos = player.getPosition();
+	sf::Vector2i curserPos = sf::Mouse::getPosition(window);
+
+	float dx = curserPos.x - playerPos.x;
+	float dy = curserPos.y - playerPos.y;
+
+	float rotation = atan2(dy, dx) * 180 / PI + 90;
+	player.setRotation(rotation);
+}
+
+//Control bullet
+void handleBullet(Player& player, sf::RenderWindow& window, sf::Texture& texture, int time) {
+
+	if (time % 100 == 0)
+		AVAILABLE = true;
+
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && AVAILABLE) {
+		Player bullet(texture);
+		bullet.setPosition(player.getPosition());
+		handleRotation(bullet, window);
+		bullets.push_back(bullet);
+		AVAILABLE = false;
+	}
+
+	for (int i = 0; i < bullets.size(); i++) {
+		Player& bullet = bullets[i];
+		if (bullet.getPosition().x < 0 || bullet.getPosition().x > WIDTH || bullet.getPosition().y < 0 || bullet.getPosition().y > HEIGHT) {
+			bullets.erase(bullets.begin());
+			i--;
+		}
+		else {
+
+			float angle;
+			sf::Vector2f velocity(0, 0);
+			if (bullet.getRotation() >= 270) {
+				angle = bullet.getRotation() - 270;
+				velocity.x = -cosf(angle * PI / 180) * BULLET_SPEED;
+				velocity.y = -sinf(angle * PI / 180) * BULLET_SPEED;
+			}
+			else if (bullet.getRotation() >= 180) {
+				angle = bullet.getRotation() - 180;
+				velocity.x = -sinf(angle * PI / 180) * BULLET_SPEED;
+				velocity.y = cosf(angle * PI / 180) * BULLET_SPEED;
+			}
+			else if (bullet.getRotation() >= 90) {
+				angle = bullet.getRotation() - 90;
+				velocity.x = cosf(angle * PI / 180) * BULLET_SPEED;
+				velocity.y = sinf(angle * PI / 180) * BULLET_SPEED;
+			}
+			else {
+				angle = bullet.getRotation();
+				velocity.x = sinf(angle * PI / 180) * BULLET_SPEED;
+				velocity.y = -cosf(angle * PI / 180) * BULLET_SPEED;
+			}
+
+			bullet.move(velocity);
+			window.draw(bullet);
+		}
+	}
 }
 
 //Get texture from file
