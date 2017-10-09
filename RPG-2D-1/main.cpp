d87e6380a60bb0bcc6e622ce625202416286a93b
Wei Tung Chen <lleeoochen@gmail.com>
2017-10-08 17:23:53 -0700

    Added sprites.

--- RPG-2D-1/main.cpp
+++ RPG-2D-1/main.cpp
@@ -2,19 +2,33 @@
 #include <iostream>
 #include <math.h>
 
-const float SPEED = 0.3;
-const int WIDTH = 700;
-const int HEIGHT = 600;
-void handleMovement(sf::CircleShape& player);
+const float SPEED = 0.5;
+const int WIDTH = 960;
+const int HEIGHT = 540;
+void handleMovement(sf::Sprite& player);
 
 //Game driver
 int main() {
 
-	//Basic setup
-	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "RPG-2D Game!", sf::Style::Titlebar | sf::Style::Default);
-	sf::CircleShape player(30.f);
-	player.setFillColor(sf::Color::Blue);
-	player.setOrigin(player.getRadius(), player.getRadius());
+	//Setup window
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Galaxy Game!", sf::Style::Titlebar | sf::Style::Default);
+
+	//Setup background
+	sf::Texture textureBackground;
+	if (!textureBackground.loadFromFile("galaxy.png"))
+		std::cerr << "galaxy.png not found" << std::endl;
+	textureBackground.setSmooth(true);
+	sf::Sprite background(textureBackground);
+
+	//Setup player
+	sf::Texture texturePlayer;
+	if (!texturePlayer.loadFromFile("ufo.png"))
+		std::cerr << "ufo.png not found" << std::endl;
+	texturePlayer.setSmooth(true);
+	sf::Sprite player(texturePlayer);
+	player.setOrigin(player.getGlobalBounds().width / 2, player.getGlobalBounds().height / 2);
+	player.setPosition(WIDTH / 2, HEIGHT - player.getGlobalBounds().height / 2);
+
 
 	//Game loop
 	while (window.isOpen()) {
@@ -31,6 +45,7 @@ int main() {
 
 		//Draw objects
 		window.clear();
+		window.draw(background);
 		window.draw(player);
 		window.display();
 	}
@@ -39,17 +54,17 @@ int main() {
 }
 
 //Control player movement
-void handleMovement(sf::CircleShape& player) {
+void handleMovement(sf::Sprite& player) {
 	sf::Vector2f velocity(0.f, 0.f);
 
 	//Four movement direction and boundary check
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player.getPosition().x - player.getRadius() >= 0)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && player.getPosition().x - player.getGlobalBounds().width / 2 >= 0)
 		velocity.x -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player.getPosition().x + player.getRadius() <= WIDTH)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && player.getPosition().x + player.getGlobalBounds().width / 2 <= WIDTH)
 		velocity.x += SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y - player.getRadius() >= 0)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && player.getPosition().y - player.getGlobalBounds().height / 2 >= 0)
 		velocity.y -= SPEED;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y + player.getRadius() <= HEIGHT)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && player.getPosition().y + player.getGlobalBounds().height / 2 <= HEIGHT)
 		velocity.y += SPEED;
 
 	//Diagonal movement (/sqrt(2))
